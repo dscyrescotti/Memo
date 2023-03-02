7a5bb6a16b42eb65ceb2cf3e61d6d7bf2bfe16e8
dscyrescotti <dscyrescotti@gmail.com>
2023-03-02 13:39:20 +0630

    commit initially

---
+++ Memo/App/MemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoApp.swift
+//  Memo
+//
+//  Created by Aye Chan on 3/2/23.
+//
+
+import SwiftUI
+
+@main
+struct MemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RecordingsView()
+        }
+    }
+}
---
+++ Memo/Features/Recordings/RecordingsView.swift
@@ -0,0 +1,20 @@
+//
+//  RecordingsView.swift
+//  Memo
+//
+//  Created by Aye Chan on 3/2/23.
+//
+
+import SwiftUI
+
+struct RecordingsView: View {
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                Text("Hello Recodings")
+            }
+            .navigationTitle("Memo")
+            .navigationBarTitleDisplayMode(.large)
+        }
+    }
+}
